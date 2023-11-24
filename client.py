1dcb5c7f46526d9f641889522cc2954814cfc7a1
Collins <upendocollins12@gmail.com>
2023-11-24 07:24:32 +0300

    complete server code

--- client.py
+++ client.py
@@ -1,1 +1,8 @@
 import socket
+
+SERVER_IP = socket.gethostbyname(socket.gethostname())
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect((SERVER_IP, 6007)) # Connect to the address defined in the server script
+
--- server.py
+++ server.py
@@ -11,3 +11,44 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to our server IP address on port 6007
 # the bind method takes a tuple in this format (ADDRESS, PORT)
 server_socket.bind((SERVER_IP, 6007))
+
+
+# The function below will run in a new thread
+# It will handle incoming socket communications from clients
+def client_handler(client_socket, client_address):
+    print(f"New connection: {client_address}")
+    
+    while True:
+        message = client_socket.recv(128).decode("utf-8")
+        # message_length = int(message_length)
+        # message = client_socket.recv(message_length).decode("utf-8")
+        # The block above waits to receive a 128 byte message from the client
+        # The message is then decoded from bytes to utf8 format
+
+        print(f"{client_address} : {message}") # Print the message sent by the client
+
+        # Cleanly disconnect the client if the message is #?DisconnectSignal?#
+        if message == "#?DisconnectSignal?#":
+            break # Stop the loop
+
+    client_socket.close()
+    print(f"{client_address} disconnected")
+
+
+server_socket.listen()
+
+# Run an infinite loop
+while True:
+    # Wait for a new connection, then capture the client
+    # socket object and client address (IP address + Port)
+    # into variables
+    client_socket, client_address = server_socket.accept()
+
+    # Run the handler function on a new thread (t)
+    # Pass the variables above to the handler
+    t = threading.Thread(target=client_handler, args=(client_socket, client_address))
+    t.start() # Start the thread
+
+    # Output the total number of connections
+    print(f"The server is handling {threading.activeCount() - 1} connections")
+
